92f22baab8db01c13a5f2b673448c610cd34211a
fazlullahb0 <fazlullahb0@gmail.com>
2022-12-10 20:20:08 +0530

    updated

---
+++ C_programming/variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(){ 
+	printf("hello world \n");
+	int total_life = 10;
+	int userd_life = 4;
+	int remaining_life = total_life - userd_life;
+	printf("The remaining lifes are: %d \n", remaining_life);
+	
+	getchar();
+	return 0;
+}
