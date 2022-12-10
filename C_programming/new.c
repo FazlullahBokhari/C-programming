02e97a55589b3a57c8a9df3ebeba237074419bb3
fazlullahb0 <fazlullahb0@gmail.com>
2022-12-10 20:09:13 +0530

    updated

---
+++ C_programming/new.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "limits.h"
+
+int main(int argc, const char * argv[]){
+	// insert code here... 
+	printf("hello world");
+	printf("Value for storage size of float: %d \n ", sizeof(float));
+	getchar();
+
+	return 0;
+}
